db3dbc9cd0ada3bfa4897ca383826e32e8b486ef
mahumahmed <mahum.a901@gmail.com>
2026-06-19 14:07:18 -0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,47 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-stacks",
-      "name": "stacks",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 960,
-      "initialViewParameters": {
-        "yaw": -0.019062638422431277,
-        "pitch": 0.5433115968938189,
-        "fov": 1.4010588678110822
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.0273447996503986,
-          "pitch": 0.18638862239372678,
-          "rotation": 0,
-          "target": "2-dsc_entry"
-        },
-        {
-          "yaw": -3.0912811892228884,
-          "pitch": 0.15062072529518744,
-          "rotation": 0,
-          "target": "1-3rd_floor_foyer"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-3rd_floor_foyer",
+      "id": "0-3rd_floor_foyer",
       "name": "3rd_floor_foyer",
       "levels": [
         {
@@ -69,7 +29,47 @@ var APP_DATA = {
           "yaw": 0.1093478972423867,
           "pitch": 0.20780756645136478,
           "rotation": 0,
-          "target": "0-stacks"
+          "target": "1-stacks"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-stacks",
+      "name": "stacks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 0.08513912071843421,
+        "pitch": 0.21663855426846723,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0273447996503986,
+          "pitch": 0.18638862239372678,
+          "rotation": 0,
+          "target": "2-dsc_entry"
+        },
+        {
+          "yaw": -3.0912811892228884,
+          "pitch": 0.15062072529518744,
+          "rotation": 0,
+          "target": "0-3rd_floor_foyer"
         }
       ],
       "infoHotspots": []
@@ -103,7 +103,7 @@ var APP_DATA = {
           "yaw": 2.93074936850945,
           "pitch": 0.19109062217176742,
           "rotation": 0,
-          "target": "0-stacks"
+          "target": "1-stacks"
         }
       ],
       "infoHotspots": []
@@ -111,8 +111,8 @@ var APP_DATA = {
   ],
   "name": "Project Title",
   "settings": {
-    "mouseViewMode": "qtvr",
-    "autorotateEnabled": true,
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
     "fullscreenButton": true,
     "viewControlButtons": false
   }
