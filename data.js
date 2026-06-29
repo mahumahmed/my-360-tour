53245a8139680c25bf1a117e54a75acd8fb57d63
mahumahmed <mahum.a901@gmail.com>
2026-06-29 14:08:53 -0700

    Add files via upload

--- data.js
+++ data.js
@@ -32,7 +32,14 @@ var APP_DATA = {
           "target": "1-stacks"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.0,
+          "pitch": 0.0,
+          "title": "Watch Video",
+          "text": "<iframe width='100%' height='200' src='https://www.youtube.com/embed/G51MmepmAQA?si=FHc5dpqzIkPiTpY_' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>"
+        }
+      ]
     },
     {
       "id": "1-stacks",
